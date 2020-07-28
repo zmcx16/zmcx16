24486ac4bc4733371e170edc4b6900f2b57778d0
zmcx16 <zmcx16@gmail.com>
2020-07-28 23:06:26 +0800

    add get blog posts on build_readme.py

---
+++ README-basic.md
@@ -0,0 +1,7 @@
+![alt README header](https://github.com/zmcx16/zmcx16/blob/master/images/kanban1-demo.jpg?raw=true)
+
+## Blog Posts
+<!-- blog start -->
+{blog content}
+<!-- blog end -->
+More on [blog.zmcx16.moe](https://blog.zmcx16.moe/)
--- build_readme.py
+++ build_readme.py
@@ -1,8 +1,66 @@
+import os
 import pathlib
+import json
+import requests
+from dateutil import parser
 
 
-root = pathlib.Path(__file__).parent.resolve()
+def send_request(url):
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+    except Exception as exc:
+        print('Generated an exception: %s' % exc)
+        return -1, exc
+
+    return 0, res.text
+
+
+def string_2_markdown(data):
+    temp_data = data
+    temp_data = temp_data.replace('[', '\[')
+    temp_data = temp_data.replace(']', '\]')
+    return temp_data
+
+
+def parse_blog_content(post_dict):
+    posts_output = ''
+    for post in post_dict['items']:
+        post_title = string_2_markdown(post['title'])
+        post_url = post['url']
+        post_date = parser.parse(post['updated'])
+        posts_output += '* [' + post_title +'](' + post_url + ') - ' + post_date.strftime("%Y-%m-%d") + '\n'
+
+    return posts_output
+
+
+def gen_blog_content():
+    blog_basic_url = 'https://www.googleapis.com/blogger/v3/blogs/'
+    blog_post_id = os.environ.get("BLOG_POST_ID", "")
+    blog_api_key = os.environ.get("BLOG_API_KEY", "")
+
+    query_url = blog_basic_url + blog_post_id + '/posts?key=' + blog_api_key
+
+    ret, content = send_request(query_url)
+
+    if ret == 0:
+        try:
+            return parse_blog_content(json.loads(content))
+
+        except Exception as exc:
+            print('Generated an exception: %s' % exc)
+
+    return 'Service Temporarily Unavailable'
+
 
 if __name__ == "__main__":
-    readme = root / "README.md"
-    readme.open("w").write("Hello Happy World")
+    root = pathlib.Path(__file__).parent.resolve()
+    readme_path = root / "README.md"
+    readme_basic_path = root / "README-basic.md"
+
+    output = ""
+    with open(readme_basic_path, 'r', encoding='utf-8') as f:
+        output = f.read().replace('{blog content}', gen_blog_content())
+
+    with open(readme_path, 'w', encoding='utf-8') as f:
+        f.write(output)
